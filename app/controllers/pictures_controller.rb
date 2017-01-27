4e46c5ea6226c7cd055bd347bb53a1108ca8d7d3
Dgibbins <nayr_ryan@hotmail.com>
2017-01-27 13:41:33 -0500

    Configuring root as pictures#index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,11 +8,26 @@ class PicturesController < ApplicationController
   end
 
   def new
-    
+    @picture = Picture.new
+    # render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
   end
 
   def create
-    render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    # make a new picture using a private method picture_params. its a 'param sanitizer' which prevents people from messing with what values can be entered in as params.
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      #if save was successful, go to ....
+      redirect_to pictures_url
+    else
+      #render the view associated with the action :new (new.html.erb)
+      render :new
+    end
   end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 
 end
