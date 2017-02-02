beb3bb84e9fd4c6b3b6eb96f3ae0b68bcedf1cac
Dgibbins <nayr_ryan@hotmail.com>
2017-02-02 16:16:26 -0500

    validation -stretch

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,24 @@
 class Picture < ApplicationRecord
+# FIN = 20
+# INIT = 3
+# scope :range, -> { where('title > ? AND title < ?', INIT, FIN) }
+
+  validates :artist, presence: { message: "Squirrel!"},
+                    length: {minimum:1}
+
+  validates :title, presence: true
+  validate :title_range
+
+
+
+  validates :url, presence: true,
+                  uniqueness: {messages: "This url already exists"}
+
+
+  def title_range
+    return if title == nil
+    if title.length > 20 || title.length < 3
+      errors.add(:title, "Name must be between 3-20 characters")
+    end
+  end
 end
